2f03ac1a534a78b8c66275968dfa2ffdcc98d9fe
sokaman <skmdev29@gmail.com>
2018-11-05 23:39:58 +0800

    Update Koa.Context interface

--- server/types/global.d.ts
+++ server/types/global.d.ts
@@ -1,8 +1,10 @@
 import Koa from 'koa';
 import { Server } from 'next';
+import { TLogger } from '../services/Logger';
 
 export declare module 'koa' {
   interface Context {
     next: Server;
+    logger: TLogger;
   }
 }
