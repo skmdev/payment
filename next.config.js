c623e88709988f0cdd1bd24670d2c3587966d745
sokaman <skmdev29@gmail.com>
2018-11-05 20:03:11 +0800

    Add antd

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const withTypescript = require('@zeit/next-typescript');
+const withLess = require('@zeit/next-less');
+const withPlugins = require('next-compose-plugins');
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {};
+}
+
+module.exports = withPlugins([
+  withTypescript,
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        javascriptEnabled: true
+      }
+    }
+  ]
+]);
--- pages/a.tsx
+++ pages/a.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Button } from 'antd';
 import { a } from '../server/utils';
 
-export default () => <div>{a()}</div>;
+export default () => (
+  <div>
+    {a()}
+    <Button>Test</Button>
+  </div>
+);
